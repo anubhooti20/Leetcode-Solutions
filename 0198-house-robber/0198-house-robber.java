cd3dbaff16bd4c43d0cb373a712c491a963c05ae
Anubhooti Nagar <122031010+anubhooti20@users.noreply.github.com>
2025-01-07 23:13:02 +0530

    Time: 0 ms (100%), Space: 41.2 MB (40.51%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        //if only 1 element just return it
+        if(nums.length<2)
+            return nums[0];
+        
+        //create array to store the maximum loot at each index
+        int[] dp= new int[nums.length];
+
+        //memoize maximum loots at first2 indexes
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+
+        //use them to fill complete array
+        for(int i=2;i<nums.length;i++){
+
+            //core logic
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
+}
